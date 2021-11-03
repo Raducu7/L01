b195e3606f3ed84985ab8b7c4031d6c151be54d0
Radu Hirsovescu <59286953+Raducu7@users.noreply.github.com>
2021-11-03 18:31:15 +0200

    Added Tema

--- Radu Hirsovescu/L04/Student.cs
+++ Radu Hirsovescu/L04/Student.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
 
+namespace L04
+{
+    class Student : TableEntity
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public int An { get; set; }
+        public string Nr { get; set; }
+        public string Facultate { get; set; }
+
+
+        public Student(string university, string id)
+        {
+            this.PartitionKey = university;
+            this.RowKey = id;
+        }
+
+        public Student(){}
+    }
+}
